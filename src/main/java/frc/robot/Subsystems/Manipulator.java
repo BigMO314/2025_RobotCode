42d285364f5e3d8717718437620a0d15843a551b
khunterg <khgochag@gmail.com>
2025-02-15 14:00:18 -0500

    Added Manipulator

--- src/main/java/frc/robot/Subsystems/Manipulator.java
+++ src/main/java/frc/robot/Subsystems/Manipulator.java
@@ -1,5 +1,6 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -12,5 +13,29 @@ public class Manipulator {
 
     private static VictorSPX mtrManipulator_L = new VictorSPX(6);
     private static VictorSPX mtrManipulator_R = new VictorSPX(7);
+
+    private static double mPower;
+
+    private Manipulator(){}
+
+    public static void init(){
+
+        mtrManipulator_L.setInverted(true);
+        mtrManipulator_R.setInverted(false);
+
+    }
+
+    public static void setManipulatorPower(double power){
+    
+        mPower = power;
+    
+    }
+
+    public static void periodic(){
+
+        mtrManipulator_L.set(ControlMode.PercentOutput, mPower);
+        mtrManipulator_R.set(ControlMode.PercentOutput, mPower);
+
+    }
 
 }
